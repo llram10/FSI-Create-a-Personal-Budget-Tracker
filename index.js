b8a903608d37299f7c8328925dcb3d2427911769
Letticia Ramos <letticia1ramos@gmail.com>
2022-08-04 19:42:48 -0700

    added js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+window.prompt(weeklyExpensesQuestions[0])
+
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(300)
+
 // Your Code Here
